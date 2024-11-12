86721aed5c83f711cb7ebc143961ce96fe9e28bc
Adrián Leiva <122684970+adrianl01@users.noreply.github.com>
2024-11-12 17:44:16 -0300

    Delete lib/firebase2.ts

--- lib/firebase2.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-
-const app = initializeApp(firebaseConfig);
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-export { db }
