efb8554b54e838ce2c4a4e3240a64947389ef468
adrianl01 <gustavo.adrian.leiva879@gmail.com>
2024-11-23 18:53:08 -0300

    11th commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
         return [
             {
                 // matching all API routes
-                source: "/api/:path*",
+                source: "pages/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
